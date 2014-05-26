8fc5aa96a7506ef65dac5fb24fd34d703a26b983
Giulliano Álvaro <giullianoalvaro@outlook.com>
2014-05-26 16:35:44 -0300

    Projeto Inicial

---
+++ Cliquemix/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Cliquemix.Startup))]
+namespace Cliquemix
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
