069278eac9f55f345ffe2d895c81c93cfe0f20ef
Rayans Carvalho <rayans.carvalho@sicove.com.br>
2014-05-27 16:32:15 -0300

    tutooo

---
+++ Cliquemix/Models/Todos/cl_ban.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cliquemix.Models.Todos
+{
+    public class cl_ban
+    {
+        #region _Atributos 
+        private Int32 _baid; //Código do ban
+        private Int32 _btid; //Código do tipo do ban
+        private Int32 _uid; //Código do usuário banido
+        private DateTime _dt_ocorrido; //Data do ban ocorrido
+        #endregion
+
+        #region _Propriedades 
+        public int Baid
+        {
+            get { return _baid; }
+            set { _baid = value; }
+        }
+
+        public int Btid
+        {
+            get { return _btid; }
+            set { _btid = value; }
+        }
+
+        public int Uid
+        {
+            get { return _uid; }
+            set { _uid = value; }
+        }
+
+        public DateTime DtOcorrido
+        {
+            get { return _dt_ocorrido; }
+            set { _dt_ocorrido = value; }
+        }
+        #endregion
+    }
+}
---
+++ Cliquemix/Models/Todos/cl_img_premio.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cliquemix.Models.Todos
+{
+    public class cl_img_premio
+    {
+        #region _Atributos 
+        private Int32 _imgid;//Código da imagem do prêmio
+        private Int32 _pid; //Código do prêmio
+        private string _premio_img; //Caminho da imagem do prêmio
+        #endregion
+
+        #region _Propriedades 
+        public int Imgid
+        {
+            get { return _imgid; }
+            set { _imgid = value; }
+        }
+
+        public int Pid
+        {
+            get { return _pid; }
+            set { _pid = value; }
+        }
+
+        public string PremioImg
+        {
+            get { return _premio_img; }
+            set { _premio_img = value; }
+        }
+        #endregion
+    }
+}
