1d22aa0512ac14dbf112d1603e9f1a3ab5f769b0
Giulliano Álvaro <giullianoalvaro@outlook.com>
2014-06-12 11:36:10 -0300

    Implementação de views

--- Cliquemix/Controllers/LogoutController.cs
+++ Cliquemix/Controllers/LogoutController.cs
@@ -8,25 +8,27 @@ using System.Web.Security;
 namespace Test_AspNet.Controllers
 {
     public class LogoutController : Controller
-    {
-
+    {        
         public ActionResult Logout()
         {
             return View();
         }
-        
-        // POST: /Logout/Logout
-        [HttpPost]
+
+        // POST: /Logout/Sair
+        [HttpPost, ActionName("Logout")]
         [ValidateAntiForgeryToken]
         public ActionResult Logout(bool sair)
         {
-            if (sair)
-            {
-                FormsAuthentication.SignOut();
-                return RedirectToAction("PrincipalAnunciante", "PrincipalAnunciante");
-            }
-            return View();
-        }        
+            FormsAuthentication.SignOut();
+            return RedirectToAction("PrincipalAnunciante", "PrincipalAnunciante");
+        }
+
+        protected void btnlogout_Click(object sender, EventArgs e)
+        {
+            Session.Abandon();
+            Session.Clear();
+            Response.Redirect("~/PrincipalAnunciante/PrincipalAnunciante");
+        }
 
     }
 }
