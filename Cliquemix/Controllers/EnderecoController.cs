71c657f08249226139bfa244619108d253f0f6c8
Giulliano Álvaro <giullianoalvaro@outlook.com>
2014-06-26 10:14:43 -0300

    Atualizações nos controles de cep

--- Cliquemix/Controllers/EnderecoController.cs
+++ Cliquemix/Controllers/EnderecoController.cs
@@ -4,16 +4,18 @@ using System.EnterpriseServices;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Cliquemix.Models;
 
 namespace Cliquemix.Controllers
 {
     public class EnderecoController : Controller
     {
+        Endereco endereco = new Endereco();
         //
         // GET: /Endereco/
         public ActionResult Index()
         {
-            return View();
+            return View();            
         }
     }
 }
--- Cliquemix/Models/Endereco.cs
+++ Cliquemix/Models/Endereco.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Web;
+using System.Text;
 using System.Data;
 using System.Data.Common;
 using System.Data.Entity.Core.EntityClient;
@@ -12,22 +14,74 @@ namespace Cliquemix.Models
 {
     public partial class Endereco
     {
-        public string cep { get; set; }
-        public string dsPais { get; set; }
-        public string dsEstado { get; set; }
-        public string dsCidade { get; set; }
-        public string dsBairro { get; set; }
-        public string dsLogradouro { get; set; }
-
         private ApplicationDbContext db = new ApplicationDbContext();
 
+        #region _Váriavies 
+
+        private string _uf;
+        private string _cidade;
+        private string _bairro;
+        private string _tipo_lagradouro;
+        private string _lagradouro;
+        private string _resultado;
+        private string _resultato_txt;
+        public string _cep;
+
+        #endregion
+
+        #region _Propiedades 
+
+        public string cep
+        {
+            get { return _cep; }
+            set { _cep = value; }
+        }
+        public string UF
+        {
+            get { return _uf; }
+        }
+
+        public string Cidade
+        {
+            get { return _cidade; }
+        }
+
+        public string Bairro
+        {
+            get { return _bairro; }
+        }
+
+        public string TipoLagradouro
+        {
+            get { return _tipo_lagradouro; }
+        }
+
+        public string Lagradouro
+        {
+            get { return _lagradouro; }
+        }
+
+        public string Resultado
+        {
+            get { return _resultado; }
+        }
+
+        public string ResultadoTXT
+        {
+            get { return _resultato_txt; }
+        }
+
+        #endregion
+
         #region _Método Localizar Endereço {Validação CEP}
+
         public void localizarEnd(string cep)
         {
             using (EntityConnection conn = new EntityConnection("name=cliquemixEntities"))
             {
                 conn.Open();
-                string _QrySQL = @"SELECT tbCep.dsPais, tbCep.dsEstado, tbCep.dsCidade, tbCep.dsBairro, tbCep.dsLogradouro
+                string _QrySQL =
+                    @"SELECT tbCep.dsPais, tbCep.dsEstado, tbCep.dsCidade, tbCep.dsBairro, tbCep.dsLogradouro
                     FROM cliquemixEntities.tbCep 
                     WHERE tbCep.cep = @pCep";
 
@@ -46,16 +100,74 @@ namespace Cliquemix.Models
                     // Iterate through the collection of Contact items.
                     while (rdr.Read())
                     {
+                        /*
                         this.dsPais = rdr["dsPais"].ToString();
                         this.dsEstado = rdr["dsEstado"].ToString();
                         this.dsCidade = rdr["dsCidade"].ToString();
                         this.dsBairro = rdr["dsBairro"].ToString();
                         this.dsLogradouro = rdr["dsLogradouro"].ToString();
+                        */
                     }
                 }
             }
         }
+
         #endregion
 
-    }
+
+        public Endereco()
+        {
+            _uf = "";
+            _cidade = "";
+            _bairro = "";
+            _tipo_lagradouro = "";
+            _lagradouro = "";
+            _resultado = "0";
+            _resultato_txt = "CEP não encontrado";
+            _cep = string.Empty;
+        }
+
+        public void pesquisar()
+        {
+            //Cria um DataSet  baseado no retorno do XML  
+            DataSet ds = new DataSet();
+            ds.ReadXml("http://cep.republicavirtual.com.br/web_cep.php?cep=" + cep.Replace ("-","").Trim()+ "&formato=xml");
+
+            if (ds != null)
+            {
+                if (ds.Tables[0].Rows.Count > 0)
+                {
+                    _resultado = ds.Tables[0].Rows[0]["resultado"].ToString();
+                    switch (_resultado)
+                    {
+                        case "1":
+                            _uf = ds.Tables[0].Rows[0]["uf"].ToString().Trim();
+                            _cidade = ds.Tables[0].Rows[0]["cidade"].ToString().Trim();
+                            _bairro = ds.Tables[0].Rows[0]["bairro"].ToString().Trim();
+                            _tipo_lagradouro = ds.Tables[0].Rows[0]["tipo_logradouro"].ToString().Trim();
+                            _lagradouro = ds.Tables[0].Rows[0]["logradouro"].ToString().Trim();
+                            _resultato_txt = "CEP completo";
+                            break;
+                        case "2":
+                            _uf = ds.Tables[0].Rows[0]["uf"].ToString().Trim();
+                            _cidade = ds.Tables[0].Rows[0]["cidade"].ToString().Trim();
+                            _bairro = "";
+                            _tipo_lagradouro = "";
+                            _lagradouro = "";
+                            _resultato_txt = "CEP  único";
+                            break;
+                        default:
+                            _uf = "";
+                            _cidade = "";
+                            _bairro = "";
+                            _tipo_lagradouro = "";
+                            _lagradouro = "";
+                            _resultato_txt = "CEP não  encontrado";
+                            break;
+                    }
+                }
+
+            }
+        }
+}
 }
