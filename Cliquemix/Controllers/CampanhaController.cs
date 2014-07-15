2ad3b1385d37b22ed3cae64e7f0a6bfd9b0f18f5
Rayans Carvalho <rayanscs@gmail.com>
2014-07-15 13:03:39 -0300

    Atualização Campanha e credito

---
+++ Cliquemix/Controllers/CampanhaController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Test_AspNet.Controllers
+{
+    public class CampanhaController : Controller
+    {
+        // GET: Campanha
+        public ActionResult CreateCampanha()
+        {
+            return View();
+        }
+
+        public ActionResult UpdateCampanha()
+        {
+            return View();
+        }
+
+        public ActionResult ListCampanha()
+        {
+            return View();
+        }
+
+        public ActionResult DeleteCampanha()
+        {
+            return View();
+        }
+
+        public ActionResult ViewerCampanha()
+        {
+            return View();
+        }
+    }
+}
