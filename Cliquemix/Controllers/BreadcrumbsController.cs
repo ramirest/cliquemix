b2c5e63a2f4a06f78490d1f035d8c7dc3d4ef1d1
Rayans Carvalho <rayanscs@gmail.com>
2014-07-17 14:56:10 -0300

    Layout dos relatorios de Anunciante

--- Cliquemix/Controllers/BreadcrumbsController.cs
+++ Cliquemix/Controllers/BreadcrumbsController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace Test_AspNet.Controllers
+namespace Cliquemix.Controllers
 {
     public class BreadcrumbsController : Controller
     {
