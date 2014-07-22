814716e419570f990f20903533ab3c9a79b62c6e
Rayans Carvalho <rayanscs@gmail.com>
2014-07-22 14:16:16 -0300

    Perfil Anunciante

---
+++ Cliquemix/Controllers/Anunciante/PerfilAnuncianteController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cliquemix.Controllers.Anunciante
+{
+    public class PerfilAnuncianteController : Controller
+    {
+        //
+        // GET: /PerfilAnunciante/
+        public ActionResult PerfilAnunciante()
+        {
+            return View();
+        }
+
+        public ActionResult PerfilAnunciantePrincipal()
+        {
+            return PartialView();
+        }
+
+
+        public ActionResult EditarPerfilDadosEmpresa()
+        {
+            return View();
+        }
+
+        public ActionResult EditarPerfilDadosContato()
+        {
+            return View();
+        }
+
+        public ActionResult EditarPerfilDadosEndereco()
+        {
+            return View();
+        }
+
+        public ActionResult EditarPerfilDadosUsuario()
+        {
+            return View();
+        }
+	}
+}
